82f299bed7915fcd7186dc61984bb5cbaa5232a3
mucahit <mucahitakdeniz28@gmail.com>
2024-03-11 11:52:45 +0300

    finish

--- sendMail/index.js
+++ sendMail/index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 app.use(cors());
 
 require("dotenv").config();
-const myEmail = process.env?.mail;
-const password = process.env?.password;
-const PORT = process.env?.PORT;
+const myEmail = process.env?.EMAIL;
+const password = process.env?.PASSWORD;
+const PORT = process.env.PORT || 8000;
 
 // Middleware for parsing JSON data
 
@@ -23,53 +23,48 @@ app.post("/sendmail", (req, res) => {
   );
   if (name) {
     if (email) {
-      if (company) {
-        if (message) {
-          if (isEmailValidated) {
-            try {
-              let transporter = nodemailer.createTransport({
-                service: "gmail",
-                auth: {
-                  user: myEmail,
-                  pass: password,
-                },
-              });
+      if (message) {
+        if (isEmailValidated) {
+          try {
+            let transporter = nodemailer.createTransport({
+              service: "gmail",
+              auth: {
+                user: myEmail,
+                pass: password,
+              },
+            });
 
-              // Compose the email
-              let mailOptions = {
-                from: email,
-                to: myEmail,
-                subject: "New Contact Form Submission",
-                text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`,
-              };
+            // Compose the email
+            let mailOptions = {
+              from: myEmail,
+              to: myEmail,
+              subject: "New Contact Form Submission",
+              text: `Name: ${name}\n Email: ${email} \n Message: ${message}`,
+            };
 
-              // Send the email
-              transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                  console.log("ERROR", error);
-                  res.errorStatusCode = 500;
-                  throw new Error(" Internal server error ");
-                } else {
-                  console.log("SUCCESS", info);
-                  res
-                    .status(200)
-                    .json({ message: "Form submitted successfully" });
-                }
-              });
-            } catch (error) {
-              console.error(error);
-              res.status(500).json({ error: "Internal server error" });
-            }
-          } else {
-            res.status(403).json({ error: "Invalid email address" });
-            return;
+            // Send the email
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                console.log("ERROR", error);
+                res.errorStatusCode = 500;
+                throw new Error(" Internal server error ");
+              } else {
+                console.log("SUCCESS", info);
+                res
+                  .status(200)
+                  .json({ message: "Form submitted successfully" });
+              }
+            });
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Internal server error" });
           }
         } else {
-          res.status(400).json({ error: "Message required" });
+          res.status(403).json({ error: "Invalid email address" });
           return;
         }
       } else {
-        res.status(400).json({ error: "Company required" });
+        res.status(400).json({ error: "Message required" });
         return;
       }
     } else {
@@ -82,6 +77,9 @@ app.post("/sendmail", (req, res) => {
   }
 });
 
+app.all("/", (req, res) => {
+  res.status(200).json({ message: "It is running" });
+});
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
